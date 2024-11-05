ea1786fdf7ae8f58a1692de39a3c5f62e019687e
illu <illusaini0901@gmail.com>
2024-11-05 11:23:31 +0530

    done

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
+const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' }) // for direct storage
 dotenv.config();
 
 connectDb();
@@ -21,7 +23,7 @@ app.get('/', (req, res) => {
 // Render example routes
 app.get('/home', (req, res) => {
     res.render('home', {
-        username: "Nitesh",
+        username: "xyz",
         posts: "fiana dimkana"
     });
 });
@@ -35,10 +37,31 @@ app.get('/allusers', (req, res) => {
     });
 });
 
+
 const userRoutes = require("./routes/userRoutes");
 app.use("/api/user", userRoutes);  
 
 app.use(errorHandler);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  const upload = multer({ storage: storage })
+
+
+
+// multer
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect("/home");
+  })
+
 
 app.use("/api/registerDoctor", require("./routes/doctorsDetails"));
 
